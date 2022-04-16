141ebb86bbb206943921adb8541741cd61defbbf
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-04-16 04:42:11 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,15 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+});
+
+$(function() {
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+});
